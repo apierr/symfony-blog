59c125d9608cc40e73426f4a8522a7f98988ea6f
Antonio Pierro <antonio.pierro@gmail.com>
2014-06-10 10:50:44 +0200

    fixed PHP coding standad

--- src/Blog/CoreBundle/Controller/PostController.php
+++ src/Blog/CoreBundle/Controller/PostController.php
@@ -6,9 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * PostController class
+ */
 class PostController extends Controller
 {
     /**
+     * Show the Post list
+     *
+	 * @return Array
+     *
      * @Route("/")
      * @Template()
      */
