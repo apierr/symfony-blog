5a9c9906c21d6a22bc18145108e04821cb63bc34
Antonio Pierro <antonio.pierro@gmail.com>
2014-06-13 10:34:40 +0200

    added a test for show post

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -22,4 +22,25 @@ class PostControllerTest extends WebTestCase
 
         $this->assertCount(3, $crawler->filter('h2'), 'There should be 3 displayed posts');
     }
+
+    /**
+     * tests show post
+     */
+    public function testShow()
+    {
+        $client = static::createClient();
+
+        /* @var Post $post */ 
+        $post = $client->getContainer()
+            ->get('doctrine')
+            ->getManager()
+            ->getRepository('ModelBundle:Post')
+            ->findFirst();
+
+        $crawler = $client->request('GET', '/'.$post->getSlug());
+
+        $this->assertTrue($client->getResponse()->isSuccessful(), 'The response was not successful');
+
+        $this->assertEquals($post->getTitle(), $crawler->filter('h1')->test(), 'Invalid post title');
+    }
 }
