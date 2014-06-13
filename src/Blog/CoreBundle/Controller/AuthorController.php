f18d9d5fbef0c67180f40b40c905d0e339cf726e
Antonio Pierro <antonio.pierro@gmail.com>
2014-06-13 13:02:05 +0200

    added the author controller action

--- src/Blog/CoreBundle/Controller/AuthorController.php
+++ src/Blog/CoreBundle/Controller/AuthorController.php
@@ -12,11 +12,38 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class AuthorController extends Controller
 {
     /**
-     * @Route("/show")
+     * Show posts by author
+     *
+     * @param string $slug
+     *
+     * @throws NotFoundException
+     * @return array
+     *
+     * @Route("/author/{slug}")
      * @Template()
      */
-    public function showAction()
+    public function showAction($slug)
     {
+    	$author = $this->getDoctrine()->getRepository('ModelBundle:Author')->findOneBy(
+    		array(
+    			'slug' => $slug
+    		)
+    	);
+
+    	if (null === $author) {
+    		throw $this->createNotFoundException('Author was not found');
+    	}
+
+    	$author = $this->getDoctrine()->getRepository('ModelBundle:Post')->findBy(
+    		array(
+    			'author' => $author
+    		)
+    	);
+
+    	return array(
+    		'author' => $author,
+    		'posts' => $posts
+    	);
     }
 
 }
