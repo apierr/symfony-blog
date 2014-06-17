17c3b4d454267f271983ae32c4661536ed3e2510
Antonio Pierro <antonio.pierro@gmail.com>
2014-06-17 10:42:08 +0200

    added a test for the comment action

--- src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
+++ src/Blog/CoreBundle/Tests/Controller/PostControllerTest.php
@@ -45,4 +45,43 @@ class PostControllerTest extends WebTestCase
 
         $this->assertGreaterThanOrEqual(1, $crawler->filter('article.comment')->count(), 'There should be at least 1 comment.');
     }
+
+    /**
+     * tests create a comment
+     */
+    public function testCreateComment()
+    {
+        $client = static::createClient();
+
+        /* @var Post $post */ 
+        $post = $client->getContainer()
+            ->get('doctrine')
+            ->getManager()
+            ->getRepository('ModelBundle:Post')
+            ->findFirst();
+
+        $crawler = $client->request('GET', '/'.$post->getSlug());
+
+        $buttonCrawlerNode = $crawler->selectButton('Send');
+
+        $form = $buttonCrawlerNode->form(array(
+            'blog_modelbundle_comment[authorName]' => 'A humble commenter',
+            'blog_modelbundle_comment[body]' => 'Hi, I am commenting about the following post'
+        ));
+
+        $client->submit($form);
+
+        $this->assertTrue(
+            $client->getResponse()->isRedirect('/'.$post->getSlug()),
+            'There was not redirection after submitting the form'
+        );
+
+        $crawler = $client->followRedirect();
+
+        $this->assertCount(
+            1,
+            $crawler->filter('html:contains("your comment was submitted successfully")').
+            'There was not any confirmation message'
+        );
+    }
 }
