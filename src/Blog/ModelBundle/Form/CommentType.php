28e1ac2a8b772fafe675ba5bbf627ea7f05d5303
Antonio Pierro <antonio.pierro@gmail.com>
2014-06-17 10:41:20 +0200

    generated  new form type class for the comment entity

---
+++ src/Blog/ModelBundle/Form/CommentType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Blog\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('authorName')
+            ->add('body')
+            ->add('post')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Blog\ModelBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'blog_modelbundle_comment';
+    }
+}
